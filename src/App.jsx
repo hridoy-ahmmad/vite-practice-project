02c91e537d12f8e1c92e2ab5cb3bd6592dc6745d
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-03-17 00:38:45 +0600

    Create nav with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.css'
+import Nav from './Components/Header/Nav'
 
 function App() {
 
-
   return (
     <div>
-
+      <Nav></Nav>
     </div>
   )
 }
---
+++ src/Components/Header/Nav.jsx
@@ -0,0 +1,93 @@
+import { MenuIcon, X } from 'lucide-react';
+import React, { useState } from 'react';
+
+
+const navs = [
+    {
+        id: 1,
+        name: "Home",
+        path: "/"
+    },
+    {
+        id: 2,
+        name: "Services",
+        path: "/services",
+        submenu: [
+            { name: "Web Development", path: "/services/web" },
+            { name: "UI Design", path: "/services/ui" }
+        ]
+    },
+    {
+        id: 3,
+        name: "Blog",
+        path: "/blog"
+    },
+    {
+        id: 4,
+        name: "Contact",
+        path: "/contact"
+    }
+];
+
+
+const Nav = () => {
+    const [open, setOpen] = useState(false)
+    const [active, setActive] = useState('/')
+
+    return (
+        <div>
+            <div className=' py-5 bg-black/30 backdrop-blur-md shadow-2xl p-5 text-white'>
+                <nav className='flex justify-between items-center container mx-auto'>
+                    <div className='text-2xl font-bold'>Logo</div>
+                    <ul className={`md:flex  gap-4 hidden font-semibold text-md`}>
+                        {
+                            navs.map(nav =>
+                                <li key={nav.id}
+                                    onClick={() => {
+                                        setActive(nav.path)
+                                    }
+                                    }
+                                    className={`${active === nav.path ? 'border-b-3 border-green-500' : ''} cursor-pointer`}
+                                >{nav.name}</li>
+                            )
+                        }
+                    </ul>
+                    <button className='md:hidden' onClick={() => setOpen(current => !current)}>
+                        {
+                            open ? <X></X> : <MenuIcon></MenuIcon>
+                        }
+                    </button>
+                    <div className='hidden md:flex'>
+                        <button className='btn btn-success text-white'>sign up</button>
+                    </div>
+                </nav>
+            </div>
+
+            {/* Mobile menu */}
+            <div className={`md:hidden gap-4 ${open ? "max-h-96 opacity-100" : "max-h-0 opacity-40"}    transition-all duration-500 overflow-hidden`} >
+                <ul className='w-full text-white/90 text-center pt-5 flex flex-col gap-5 text-xl font-bold bg-black/80 backdrop-blur-lg'>
+                    {
+                        navs.map(nav =>
+                            <li
+                                key={nav.id}
+                                onClick={() => {
+                                    setActive(nav.path)
+                                    setOpen(false)
+                                }
+                                }
+                                className={`${active === nav.path ? 'text-green-500' : ''}`}
+                            >{nav.name}</li>
+                        )
+                    }
+                    <div>
+                        <button className='btn btn-success text-white inline my-5'>sign up</button>
+                    </div>
+                </ul>
+
+            </div>
+        </div>
+
+    );
+};
+
+export default Nav;

df2f8187e1a7df779a55f16850345c567561f596
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-03-17 13:15:35 +0600

    fix some error

--- src/Components/Pricing/PricingCard.jsx
+++ src/Components/Pricing/PricingCard.jsx
@@ -3,7 +3,7 @@ import { CircleCheckBig } from 'lucide-react';
 import React from 'react';
 
 const PricingCard = ({ data, handleAddCart }) => {
-    const { plan_name, duration, price_usd, des, features, popular } = data
+    const { plan_name, duration, price_usd, des, features, popular , id} = data
     return (
         <div className="card bg-base-100 shadow-sm hover:shadow-2xl transition-all duration-300">
             <div className="card-body flex flex-col justify-between relative">
@@ -24,17 +24,22 @@ const PricingCard = ({ data, handleAddCart }) => {
                         popular && <span className="badge badge-xs badge-warning">Most Popular</span>
                     }
                 </div>
+
+
+
+
+                
                 <div className="mt-6 flex items-center justify-between">
                     <button onClick={() => handleAddCart(data)}
                         className="btn btn-primary  focus:scale-110">Add to Cart</button>
                     <button
-                        onClick={() => document.getElementById('my_modal_5').showModal()} className="btn btn-primary  focus:scale-110 ">view Details</button>
+                        onClick={() => document.getElementById(id).showModal()} className="btn btn-primary  focus:scale-110 ">view Details</button>
                 </div>
             </div>
 
 
             {/* Open the modal using document.getElementById('ID').showModal() method */}
-            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+            <dialog id={`${id}`} className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
                     <div className="card-body flex flex-col justify-between relative">
                         <div className="flex flex-col ">

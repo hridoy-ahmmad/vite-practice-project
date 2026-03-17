1e08b9438cde25c0b3603ef8ae7fe46fb95cd69b
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-03-17 12:59:09 +0600

    add modal

--- src/Components/Pricing/PricingCard.jsx
+++ src/Components/Pricing/PricingCard.jsx
@@ -16,30 +16,63 @@ const PricingCard = ({ data, handleAddCart }) => {
                         <span className="text-xl font-semibold">${price_usd}</span>
 
                     </div>
+                    <p className='mt-2 font-medium'>{des}</p>
 
-                    <div className=''>
-                        <p className='mt-2 font-medium'>{des}</p>
-                        <ul className="mt-6 flex flex-col gap-2 text-xs shadow-lg p-4 rounded-sm">
-                            {
-                                features.map(feature =>
-                                    <li className='flex gap-2 items-center font-medium'>
-                                        <CircleCheckBig className='text-green-600' /> {feature}
-                                    </li>)
-                            }
-
-                        </ul>
-                    </div>
                 </div>
                 <div className='absolute top-2 right-5'>
                     {
                         popular && <span className="badge badge-xs badge-warning">Most Popular</span>
                     }
                 </div>
-                <div className="mt-6 ">
+                <div className="mt-6 flex items-center justify-between">
                     <button onClick={() => handleAddCart(data)}
-                        className="btn btn-primary btn-block focus:scale-110">Add to Cart</button>
+                        className="btn btn-primary  focus:scale-110">Add to Cart</button>
+                    <button
+                        onClick={() => document.getElementById('my_modal_5').showModal()} className="btn btn-primary  focus:scale-110 ">view Details</button>
                 </div>
             </div>
+
+
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <div className="card-body flex flex-col justify-between relative">
+                        <div className="flex flex-col ">
+                            <div className='flex justify-between items-center  border-b border-b-gray-300 pb-2 '>
+                                <div>
+                                    <h2 className="text-xl font-bold ">{plan_name} </h2>
+                                    <p className='py-2 font-medium'>{duration}</p>
+                                </div>
+                                <span className="text-xl font-semibold">${price_usd}</span>
+
+                            </div>
+
+                            <div className=''>
+                                <p className='mt-2 font-medium'>{des}</p>
+                                <ul className="mt-6 flex flex-col gap-2 text-xs p-4 rounded-sm">
+                                    {
+                                        features.map(feature =>
+                                            <li className='flex gap-2 items-center font-medium'>
+                                                <CircleCheckBig className='text-green-600' /> {feature}
+                                            </li>)
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                        <div className='absolute top-2 right-5'>
+                            {
+                                popular && <span className="badge badge-xs badge-warning">Most Popular</span>
+                            }
+                        </div>
+                    </div>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 };
